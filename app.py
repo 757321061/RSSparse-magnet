44694fcba465075092a3aed6c8409af6bc14220a
757321061 <49135603+757321061@users.noreply.github.com>
2023-09-27 00:48:42 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+﻿from flask import Flask, render_template, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        # 处理表单提交
+        subscription_text = request.form['subscription_text']
+        
+        # 将输入框的内容替换到 rss.py 文件中的网页 RSS 订阅地址
+        with open('rss.py', 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+        with open('rss.py', 'w', encoding='utf-8') as file:
+            for line in lines:
+                if 'rss_url =' in line:
+                    file.write(f'rss_url = "{subscription_text}"\n')
+                else:
+                    file.write(line)
+        
+        # 运行 rss.py 文件，并获取输出内容
+        output = subprocess.check_output(['python3', 'rss.py']).decode('utf-8')
+        
+        # 在网页上显示输出内容
+        return render_template('index.html', output=output)
+    
+    # 渲染包含输入框和确定按钮的模板
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=12589)
---
+++ rss.py
@@ -0,0 +1,46 @@
+﻿import feedparser
+import re
+import urllib.parse
+
+def hash_to_magnet(hash_value):
+    base_url = 'magnet:?xt=urn:btih:'
+    encoded_hash = urllib.parse.quote(hash_value)
+    magnet_link = base_url + encoded_hash
+    return magnet_link
+
+def get_magnet_links(rss_url, filters, excludes):
+    # 解析RSS订阅
+    feed = feedparser.parse(rss_url)
+    magnet_links = []
+
+    # 遍历所有的条目
+    for entry in feed.entries:
+        # 获取标题，并检查是否满足过滤规则和排除规则
+        title = entry.get('title', '')
+        if all(filter_rule in title for filter_rule in filters) and not any(exclude_rule in title for exclude_rule in excludes):
+            # 提取哈希值
+            enclosure = entry.get('enclosures')
+            if enclosure:
+                hash_value = re.search(r'/([^/]+)\.torrent', enclosure[0].get('url'))  # 修改此行
+                if hash_value:
+                    hash_value = hash_value.group(1)
+                    magnet_link = hash_to_magnet(hash_value)
+                    magnet_links.append(magnet_link)
+
+    return magnet_links
+
+# 替换为你的RSS订阅网址
+rss_url = "https://mikanani.me/RSS/Bangumi?bangumiId=2972&subgroupid=243"
+
+# 定义过滤规则
+filters = []
+
+# 定义排除规则
+excludes = []  # 按需添加排除规则
+
+# 获取磁力链接
+links = get_magnet_links(rss_url, filters, excludes)
+
+# 打印磁力链接
+for link in links:
+    print(link)
